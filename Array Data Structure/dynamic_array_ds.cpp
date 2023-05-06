8532a81f6cd8a41eac0dc95e5be63cdca5da7ff6
Pratham <88399872+Pratham1001@users.noreply.github.com>
2023-05-06 18:34:52 +0000

    constructor

--- Array Data Structure/dynamic_array_ds.cpp
+++ Array Data Structure/dynamic_array_ds.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+//define a class Array with member variables to store capacity of array, last index of the last filled block of the array and a pointer
+class Array{
+    private:
+        int capacity;
+        int lastIndex;
+        int *ptr;
+    public:
+        Array(int cap);
+
+};
+
+//access constructor outside the class
+Array::Array(int cap)
+{
+    
+
+
+}
+
+
 int main(){
 
 
